a07b475b21916442b7beb075456716d65c77611c
tabacz <tak_to_jest@poczta.fm>
2016-05-11 00:10:59 +0200

    Wyświetlanie widoków - początki
    Wyświetlanie widoków - początki
    Stworzenie osobnej klasy Obraz i Zapytanie

---
+++ Artgram/Zapytanie.cs
@@ -0,0 +1,25 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Artgram
+{
+    class Zapytanie
+    {
+        public string ID_Kategorii;
+
+        public Zapytanie(string ID_Kategorii)
+        {
+            this.ID_Kategorii = ID_Kategorii;
+        }
+        
+    }
+}
